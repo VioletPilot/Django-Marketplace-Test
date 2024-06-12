c59a29271af5de0f0b2a12d5e1cd8810eeb0c2db
VioletPilot <sherwin.gopalkista06@gmail.com>
2024-06-12 07:40:42 +0200

    Completed Items chapter

--- Mark/core/views.py
+++ Mark/core/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from item.models import Category,Item
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    item= Item.objects.filter(is_sold=False)[0:6]
+    category=Category.objects.all()
+
+    return render(request, 'core/index.html',{
+        'categories':category,
+        'items':item,
+
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
